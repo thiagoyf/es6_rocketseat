91bd56cae8f8e9781c55c6bf2d50388b6ab4dab7
thiagoyf <thiagoyudifukunaga@gmail.com>
2019-01-24 07:24:16 -0300

    commiting changes on main.js

--- main.js
+++ main.js
@@ -1,8 +1,30 @@
-class Teste {
-  metodo() {
-
+class List {
+  constructor() {
+    this.data = [];
   }
-  outro() {
 
+  add(data) {
+    this.data.push(data);
+    console.log(this.data);
   }
 }
+
+class TodoList extends List{
+  constructor() {
+    super();
+
+    this.usuario = 'Thiago'
+  }
+
+  mostraUsuario() {
+    console.log(this.usuario);
+  }
+}
+
+var MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function() {
+  MinhaLista.add('Novo todo');
+}
+
+MinhaLista.mostraUsuario();
