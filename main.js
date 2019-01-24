a1b8d807cb5a0234d34d748dbe14e0112ed5a3a8
thiagoyf <thiagoyudifukunaga@gmail.com>
2019-01-24 07:31:38 -0300

    the static method can be used without the object be created, so he may not see the attributes non static from the class

--- main.js
+++ main.js
@@ -1,30 +1,7 @@
-class List {
-  constructor() {
-    this.data = [];
-  }
-
-  add(data) {
-    this.data.push(data);
-    console.log(this.data);
+class Matematica {
+  static soma(a, b) {
+    return a + b;
   }
 }
 
-class TodoList extends List{
-  constructor() {
-    super();
-
-    this.usuario = 'Thiago'
-  }
-
-  mostraUsuario() {
-    console.log(this.usuario);
-  }
-}
-
-var MinhaLista = new TodoList();
-
-document.getElementById('novotodo').onclick = function() {
-  MinhaLista.add('Novo todo');
-}
-
-MinhaLista.mostraUsuario();
+console.log(Matematica.soma(1, 2));
