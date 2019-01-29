1ca20fae7ee0071313217f30dc86747105f09129
thiagoyf <thiagoyudifukunaga@gmail.com>
2019-01-29 20:05:18 -0300

    commiting part that render static items

---
+++ cap4/src/main.js
@@ -0,0 +1,56 @@
+class App {
+  constructor() {
+    this.repositories = [];
+
+    this.formEl = document.getElementById('repo-form');
+    this.listEl = document.getElementById('repo-list');
+
+    this.registerHandlers();
+  }
+
+  registerHandlers() {
+    this.formEl.onsubmit = event => this.addRepository(event);
+  }
+
+  addRepository(event) {
+    event.preventDefault();
+
+    this.repositories.push({
+      name: 'rocketseat.com.br',
+      description: 'Tire sua ideia do papel e dê vida à sua startup.',
+      avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
+      html_url: 'https://github.com/rocketseat/comunidade'
+    });
+
+    this.render();
+  }
+
+  render() {
+    this.listEl.innerHTML = '';
+
+    this.repositories.forEach(repo => {
+      let imgEl = document.createElement('img');
+      imgEl.setAttribute('src', repo.avatar_url);
+
+      let titleEl = document.createElement('strong');
+      titleEl.appendChild(document.createTextNode(repo.name));
+
+      let descriptionEL = document.createElement('p');
+      descriptionEL.appendChild(document.createTextNode(repo.description));
+
+      let linkEl = document.createElement('a');
+      linkEl.setAttribute('target', '_blank');
+      linkEl.appendChild(document.createTextNode('Acessar'));
+
+      let listItemEl = document.createElement('li');
+      listItemEl.appendChild(imgEl);
+      listItemEl.appendChild(titleEl);
+      listItemEl.appendChild(descriptionEL);
+      listItemEl.appendChild(linkEl);
+
+      this.listEl.appendChild(listItemEl);
+    });
+  }
+}
+
+new App();
