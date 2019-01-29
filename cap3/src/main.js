0ea4df1492289df261d903754bd896e517a8a3fd
thiagoyf <thiagoyudifukunaga@gmail.com>
2019-01-29 06:55:06 -0300

    using async and await to make promises and axios to request info

--- cap3/src/main.js
+++ cap3/src/main.js
@@ -1,14 +1,19 @@
 /* para utilizar o async e o await é preciso fazer download do @babel/plugin-transform-async-to-generator 
 e @babel/polyfill para ser executado antes do main */
 
-const minhaPromise = () => new Promise((resolve, reject) => {
-  setTimeout(() => { resolve('OK') }, 2000);
-});
+import axios from 'axios';
 
-async function executaPromise() {
-  const response = await minhaPromise();
+class Api {
+  static async getUserInfo(username) {
+    try {
+      const response = await axios.get(`https://api.github.com/users/${username}`);
 
-  console.log(response);
+      console.log(response);
+    } catch(err) {
+      console.log('Erro na API')
+    }
+  }
 }
 
-executaPromise();
+Api.getUserInfo('thiagoyf');
+Api.getUserInfo('thiagofukunaga');
